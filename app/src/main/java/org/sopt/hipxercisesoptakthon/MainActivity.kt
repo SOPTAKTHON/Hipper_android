872603ee06914d98c6eaab94413bc93d88446723
Han Jinhee <h.wlsgml19@gmail.com>
2021-05-23 04:51:39 +0900

    mainActivity

--- app/src/main/java/org/sopt/hipxercisesoptakthon/MainActivity.kt
+++ app/src/main/java/org/sopt/hipxercisesoptakthon/MainActivity.kt
@@ -2,34 +2,50 @@ package org.sopt.hipxercisesoptakthon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
+import androidx.core.view.marginTop
+import org.sopt.hipxercisesoptakthon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG = "FirebaseService"
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-                return@OnCompleteListener
-            }
+        alarmButtonClickEvent()
+        exerciseButtonClickEvent()
 
-            Log.w(TAG, "Fetching FCM registration token success", task.exception)
 
-            // Get new FCM registration token
-            val token = task.result
-//
-//            // Log and toast
-//            val msg = getString("token", token)
-//            Log.d(TAG, msg)
-//            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-        })
+    }
+
+
+    private fun alarmButtonClickEvent(){
+        binding.imageviewButtonAlarm.setOnClickListener {
+            it.isSelected = !it.isSelected
+            //binding.imageviewButtonAlarm.marginTop(140F)
+
+            //커스텀 토스트
+            var v1 = layoutInflater.inflate(R.layout.custom_toast, null)
+            var t2 = Toast(this)
+            t2.view = v1
+            t2.show()
+
+            //다이얼로그 띄우기
+        }
+
+
+
+    }
+
+    private fun exerciseButtonClickEvent(){
+        binding.imageviewButtonExercise.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
+
+        //다이얼로그 띄우기
+
     }
 }
